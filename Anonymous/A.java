dae29735d324cf71bcdfbdf562ccfcca197b93ca
robertwsy <35885672+robertwsy@users.noreply.github.com>
2018-05-02 22:19:44 +0800

    Anonymous
    Anonymous class

---
+++ Anonymous/A.java
@@ -0,0 +1,6 @@
+package Ni;
+
+public interface A {
+	public void a();
+
+}
---
+++ Anonymous/B.java
@@ -0,0 +1,12 @@
+package Ni;
+
+public class B implements A{
+
+	@Override
+	public void a() {
+		// TODO Auto-generated method stub
+		System.out.println("只使用一次");
+	}
+	
+
+}
